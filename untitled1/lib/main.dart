7c533b9fe2b0bfdd43dc113cd823a8ae5a2e4cf7
linah31 <73790921+linah31@users.noreply.github.com>
2022-08-23 21:58:15 +0200

    ..

---
+++ untitled1/lib/main.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MyApp2());
+}
+
+class MyApp2 extends StatefulWidget {
+  const MyApp2({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp2> createState() => _MyApp2State();
+}
+
+class _MyApp2State extends State<MyApp2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
