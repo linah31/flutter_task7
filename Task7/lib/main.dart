eba27ace5e71fd289f715d50e1424f36f2214aa4
linah31 <73790921+linah31@users.noreply.github.com>
2022-08-23 22:05:39 +0200

    #1

--- Task7/lib/main.dart
+++ Task7/lib/main.dart
@@ -15,9 +15,47 @@ class MyApp2 extends StatefulWidget {
 }
 
 class _MyApp2State extends State<MyApp2> {
+  Future getimg(ImageSource source) async{
+    final image= await ImagePicker().pickImage(source:source);
+    if(image==null) return;
+
+    final imgtmp=File(image.path);
+
+    setState(() {
+      this._image=imgtmp;
+
+    });
+  }
+  File? _image;
   @override
   Widget build(BuildContext context) {
     return Container();
   }
 }
 
+
+Widget picbtns({
+  required String title,
+  required IconData icon,
+  required VoidCallback onclick,
+}){
+
+
+  return Container(
+    width: 280,
+    child:  ElevatedButton(
+      onPressed: onclick,
+      child: Row(
+        children: [
+          Icon(icon),
+          SizedBox(
+            width: 20,
+          ),
+          Text(title),
+
+        ],
+
+      ),
+    ),
+  );
+}
